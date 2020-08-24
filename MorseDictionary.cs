e90f07b65e41aac27c5c5e87919f0baeb6eec79e
Hamlet Sanchez <hpsanchez@live.com>
2020-08-23 20:11:32 -0400

    Add project files.

---
+++ MorseDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morse
+{
+    public class MorseDictionary
+    {
+        public Dictionary<char, string> morseDictionary = new Dictionary<char, string>();
+
+        public MorseDictionary()
+        {
+            morseDictionary.Add('A', ".-");
+            morseDictionary.Add('B', "-...");
+            morseDictionary.Add('C', "-.-.");
+            morseDictionary.Add('D', "-..");
+            morseDictionary.Add('E', ".");
+            morseDictionary.Add('F', "..-.");
+            morseDictionary.Add('G', "--.");
+            morseDictionary.Add('H', "....");
+            morseDictionary.Add('I', "..");
+            morseDictionary.Add('J', ".---");
+            morseDictionary.Add('K', "-.-");
+            morseDictionary.Add('L', ".-..");
+            morseDictionary.Add('M', "--");
+            morseDictionary.Add('N', "-.");
+            morseDictionary.Add('O', "---");
+            morseDictionary.Add('P', ".--.");
+            morseDictionary.Add('Q', "--.-");
+            morseDictionary.Add('R', ".-.");
+            morseDictionary.Add('S', "...");
+            morseDictionary.Add('T', "-");
+            morseDictionary.Add('U', "..-");
+            morseDictionary.Add('V', "...-");
+            morseDictionary.Add('W', ".--");
+            morseDictionary.Add('X', "-..-");
+            morseDictionary.Add('Y', "-.--");
+            morseDictionary.Add('Z', "--..");
+            morseDictionary.Add('1', ".----");
+            morseDictionary.Add('2', "..---");
+            morseDictionary.Add('3', "...--");
+            morseDictionary.Add('4', "....-");
+            morseDictionary.Add('5', ".....");
+            morseDictionary.Add('6', "-....");
+            morseDictionary.Add('7', "--...");
+            morseDictionary.Add('8', "---..");
+            morseDictionary.Add('9', "----.");
+            morseDictionary.Add('0', "-----");
+            morseDictionary.Add('.', "|");
+            morseDictionary.Add(' ', "|");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dot = 300;
+            Console.Write("Intrduzca un mensaje: ");
+            string message = Console.ReadLine();
+            message = message.ToUpper();
+
+            MorseDictionary morse = new MorseDictionary();
+            foreach(var element in message)
+            {
+                for (int i = 0; i < morse.morseDictionary[element].Length; i++)
+                {
+                    Console.Write(morse.morseDictionary[element][i]);
+                    if (morse.morseDictionary[element][i] == '.')
+                    {
+                        Console.Beep(400, dot);
+                        Thread.Sleep(dot);
+                    }
+                    else
+                    {if(morse.morseDictionary[element][i] == '-')
+                        {
+                            Console.Beep(400, dot * 3);
+                        }
+
+                    }
+                }
+                Console.Write("|");
+            }
+        }
+    }
+}
